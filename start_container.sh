bd32b91703642b477b662c7f0d9b96bdd240fcd5
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2024-12-19 17:27:08 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ravikirangunnabattula/simple-python-flask-app
+docker pull ravikirangunnabattula/simple-php-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 ravikirangunnabattula/simple-python-flask-app
+docker run -d -p 80:80 ravikirangunnabattula/simple-php-app
